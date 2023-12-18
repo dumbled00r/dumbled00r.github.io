c1edb4e30b6e1f6ac8c1f57a39a0dd7766c15881
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2023-12-18 17:20:50 +0700

    update color

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ function search() {
     var parent2 = document.getElementById("parent2").value;
     var resultText = document.getElementById("result-text");
     var result = search_web(parent1, parent2);
-    
-    resultText.innerText = result;
+
+    resultText.innerHTML = result;
 }
 
 function search_web(parent1, parent2) {
-
     var childBreedData = {
         "Common": {"Common": [95, 4.8, 0.2, 0, 0], "Uncommon": [52.5, 44.9, 2.5, 0.1, 0], "Rare": [47.6, 7.3, 42.6, 2.4, 0.1], "Epic": [47.5, 2.5, 5.1, 42.5, 2.4], "Legendary": [47.5, 2.4, 0.2, 4.9, 45]},
         "Uncommon": {"Common": [52.5, 44.9, 2.5, 0.1, 0], "Uncommon": [10, 85, 4.8, 0.2, 0], "Rare": [5.1, 47.4, 44.9, 2.5, 0.1], "Epic": [5, 42.6, 7.4, 42.6, 2.4], "Legendary": [5, 42.5, 2.5, 5, 45]},
@@ -18,6 +17,20 @@ function search_web(parent1, parent2) {
     };
 
     var a = childBreedData[parent1][parent2];
-    
-    return `\tCommon: \t${a[0]}%\n\tUncommon: \t${a[1]}%\n\tRare: \t${a[2]}%\n\tEpic: \t${a[3]}%\n\tLegendary: \t${a[4]}%`;
+
+    var commonColor = 'gray';
+    var uncommonColor = 'green';
+    var rareColor = 'blue';
+    var epicColor = 'purple';
+    var legendaryColor = 'orange';
+
+    var resultString = `
+        <span style="color: ${commonColor};">\tCommon: \t${a[0]}%</span><br>
+        <span style="color: ${uncommonColor};">\tUncommon: \t${a[1]}%</span><br>
+        <span style="color: ${rareColor};">\tRare: \t${a[2]}%</span><br>
+        <span style="color: ${epicColor};">\tEpic: \t${a[3]}%</span><br>
+        <span style="color: ${legendaryColor};">\tLegendary: \t${a[4]}%</span><br>
+    `;
+
+    return resultString;
 }
