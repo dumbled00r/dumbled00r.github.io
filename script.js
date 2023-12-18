970e85aad38b7911ceb3fe0f65810ee5761dcdde
dumbled00r <82684246+dumbled00r@users.noreply.github.com>
2023-12-18 16:55:56 +0700

    update code

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ function search() {
     var parent1 = document.getElementById("parent1").value;
     var parent2 = document.getElementById("parent2").value;
     var resultText = document.getElementById("result-text");
-
-    // Simulate the Python function
     var result = search_web(parent1, parent2);
     
     resultText.innerText = result;
 }
 
 function search_web(parent1, parent2) {
-    // Simulate the Python data
+
     var childBreedData = {
         "Common": {"Common": [95, 4.8, 0.2, 0, 0], "Uncommon": [52.5, 44.9, 2.5, 0.1, 0], "Rare": [47.6, 7.3, 42.6, 2.4, 0.1], "Epic": [47.5, 2.5, 5.1, 42.5, 2.4], "Legendary": [47.5, 2.4, 0.2, 4.9, 45]},
         "Uncommon": {"Common": [52.5, 44.9, 2.5, 0.1, 0], "Uncommon": [10, 85, 4.8, 0.2, 0], "Rare": [5.1, 47.4, 44.9, 2.5, 0.1], "Epic": [5, 42.6, 7.4, 42.6, 2.4], "Legendary": [5, 42.5, 2.5, 5, 45]},
